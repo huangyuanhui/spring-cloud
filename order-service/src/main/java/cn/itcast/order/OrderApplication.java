84211e9c483540c0e02c4ebd43675f4620189d16
huangyuanhui <2278181701@qq.com>
2023-11-28 00:54:39 +0800

    Ribbon配置负载均衡方式

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,8 +1,11 @@
 package cn.itcast.order;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,12 +20,25 @@ public class OrderApplication {
 
     /**
      * Bean的创建注入只能放在配置类里边，而启动类就是配置类，所以可以放在这里
-     * 
+     *
      * @return
      */
     @Bean
+    /**
+     * 添加负载均衡注解
+     */
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
 
+
+    /**
+     * 代码方式调整负载均衡策略：随机策略
+     * @return
+     */
+    /*@Bean
+    public IRule randomRule() {
+        return new RandomRule();
+    }*/
 }
--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -20,7 +20,9 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         // 2：利用RestTemplate发送http请求，查询用户
-        String url = "http://localhost:8082/users/" + order.getUserId();
+        //String url = "http://localhost:8082/users/" + order.getUserId();
+        // 2：用服务名代替IP、端口
+        String url = "http://user-service/users/" + order.getUserId();
         User user = restTemplate.getForObject(url, User.class);
         // 3：封装user到order
         order.setUser(user);
