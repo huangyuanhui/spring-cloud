7406963506afff6c528ecf71828a366ff25ca2cc
huangyuanhui <2278181701@qq.com>
2023-11-27 20:07:16 +0800

    基于RestTemplate实现服务远程调用

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
@@ -11,6 +12,17 @@ public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+
+    /**
+     * Bean的创建注入只能放在配置类里边，而启动类就是配置类，所以可以放在这里
+     * 
+     * @return
+     */
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 
 }
